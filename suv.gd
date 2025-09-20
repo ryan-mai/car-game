4819ac92085a35c113d37365ac29885ac12a7564
Your NameRyan <inoober2008@gmail.com>
2025-09-20 19:35:14 -0400

    Made it FOV

--- suv.gd
+++ suv.gd
@@ -1,5 +1,6 @@
 extends VehicleBody3D
 
+@onready var camera: Camera3D = $Camera3D
 var max_RPM = 450
 var max_torque = 300
 var turn_speed = 3
@@ -19,6 +20,16 @@ func _physics_process(delta: float) -> void:
 	steering = lerp(steering, steering_direction * turn_amount, turn_speed * delta)
 	print(engine_force)
 	if direction != 0:
+		if direction < 0:
+			camera.position.x = 0.3
+			camera.position.y = 1.2
+			camera.position.z = -1.0
+			camera.rotation.y = 0
+		else:
+			camera.position.x = 0.3
+			camera.position.y = 1.0
+			camera.position.z = 0.55
+			camera.rotation.y = deg_to_rad(180.0)
 		engine_force = torque
 		brake = 0
 	else:
